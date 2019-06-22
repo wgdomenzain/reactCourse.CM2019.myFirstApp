d3799c239f8d69ca95047f0648ddb5843b59f329
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2019-06-22 10:25:41 -0500

    Print each name on button click

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import List from './components/List/List';
 import Button from './components/Button/Button';
 
 class App extends React.PureComponent {
-	onHandleButton = () => {};
+	state = {
+		items: [ 'Walter', 'Rocio', 'Mia', 'Walter Jr' ],
+		index: 0
+	};
+
+	onHandleButton = () => {
+		let { index, items } = this.state;
+		if (items.length > index + 1) index++;
+		else index = 0;
+		this.setState({ index: index });
+	};
 
 	render() {
+		const { items, index } = this.state;
 		return (
 			<div>
 				<p>¡Bienvenidos al curso de programación de cómputo móvil!</p>
-				<List items={[ 'Walter', 'Hola', 'oaxaca' ]} lastName={'Gonzalez'} />
+				<List items={items} lastName={'Gonzalez'} />
 				<Button label={'Aceptar'} onClick={this.onHandleButton} />
+				<p>El nombre seleccionado es: {items[index]}</p>
 			</div>
 		);
 	}
