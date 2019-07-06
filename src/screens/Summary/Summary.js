1c5038717392448f40a3067e85496470e0061ce4
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2019-07-06 14:09:21 -0500

    Chart added

--- src/screens/Summary/Summary.js
+++ src/screens/Summary/Summary.js
@@ -5,15 +5,45 @@ import summaryHeaders from '../../resources/jsons/summaryHeaders.json';
 import summaryData from '../../resources/jsons/summaryData.json';
 import { IconTable, IconChart } from '../../resources/svg/Icons';
 import produce from 'immer/dist/immer';
+import { Bar } from 'react-chartjs-2';
+import 'chartjs-plugin-annotation';
+
 export default (class Summary extends React.PureComponent {
 	state = {
 		selected: {
 			table: true,
 			chart: false
+		},
+		data: {
+			labels: [],
+			datasets: [
+				{
+					label: 'My Second dataset',
+					fillColor: 'rgba(0,191,255,0.5)',
+					strokeColor: 'rgba(0,191,255,0.8)',
+					highlightFill: 'rgba(100,149,237,0.75)',
+					highlightStroke: 'rgba(100,149,237,1)',
+					data: [ 10, 11, 12 ],
+					borderColor: 'grey',
+					borderWidth: 1
+				}
+			]
 		}
 	};
 
-	componentDidMount() {}
+	componentDidMount() {
+		this.init();
+	}
+
+	init = () => {
+		const nextState = produce(this.state, (draft) => {
+			summaryData.forEach((item, i) => {
+				draft.data.labels = draft.data.labels.concat(item.name);
+			});
+		});
+		this.setState(nextState);
+		console.log('TCL: Summary -> init -> nextState', nextState);
+	};
 
 	onHandleIcon = (item) => {
 		const nextState = produce(this.state, (draft) => {
@@ -24,8 +54,29 @@ export default (class Summary extends React.PureComponent {
 		this.setState(nextState);
 	};
 	render() {
-		const { selected } = this.state;
+		const { selected, data } = this.state;
+		const data2 = Object.assign({}, data);
+
+		console.log('TCL: Summary -> render -> data2', data2);
+		console.log('TCL: Summary -> render -> data', data);
 		const headers = summaryHeaders;
+		const options = {
+			annotation: {
+				annotations: [
+					{
+						drawTime: 'afterDatasetsDraw',
+						borderColor: 'red',
+						borderDash: [ 2, 2 ],
+						borderWidth: 2,
+						mode: 'vertical',
+						type: 'line',
+						value: 10,
+						scaleID: 'x-axis-0'
+					}
+				]
+			},
+			maintainAspectRation: false
+		};
 
 		return (
 			<div className={styles.main}>
@@ -52,7 +103,11 @@ export default (class Summary extends React.PureComponent {
 						})}
 					</div>
 				)}
-				{selected.chart && <div className={styles.chart}>Gráfica</div>}
+				{selected.chart && (
+					<div className={styles.chart}>
+						<Bar data={data2} width={100} height={50} options={options} />
+					</div>
+				)}
 			</div>
 		);
 	}
