153c6c78f16c36f42347d1630097a302d96d5d67
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2019-07-05 20:10:34 -0500

    Simple table added

--- src/screens/Tables/Tables.js
+++ src/screens/Tables/Tables.js
@@ -7,6 +7,39 @@ export default (class Tables extends React.PureComponent {
 	componentDidMount() {}
 
 	render() {
-		return <div className={styles.main}>Tablas</div>;
+		return (
+			<div className={styles.main}>
+				<table className={styles.table}>
+					<thead className={styles.mainHeader}>
+						<tr className={styles.header}>
+							<th className={styles.header_item}>Encabezado 1</th>
+							<th className={styles.header_item}>Encabezado 2</th>
+							<th className={styles.header_item}>Encabezado 3</th>
+							<th className={styles.header_item}>Encabezado 4</th>
+						</tr>
+					</thead>
+					<tbody className={styles.body}>
+						<tr className={styles.row}>
+							<td className={styles.row_item}>Item 1</td>
+							<td className={styles.row_item}>Item 2</td>
+							<td className={styles.row_item}>Item 3</td>
+							<td className={styles.row_item}>Item 4</td>
+						</tr>
+						<tr className={styles.row}>
+							<td className={styles.row_item}>Item 5</td>
+							<td className={styles.row_item}>Item 6</td>
+							<td className={styles.row_item}>Item 7</td>
+							<td className={styles.row_item}>Item 8</td>
+						</tr>
+						<tr className={styles.row}>
+							<td className={styles.row_item}>Item 1</td>
+							<td className={styles.row_item}>Item 2</td>
+							<td className={styles.row_item}>Item 3</td>
+							<td className={styles.row_item}>Item 4</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		);
 	}
 });
