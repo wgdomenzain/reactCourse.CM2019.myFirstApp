80f937bdba93ee6587b4010c61509c5e3acdd805
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2019-08-02 21:00:23 -0500

    Post added

--- src/WebServices/WebServices.js
+++ src/WebServices/WebServices.js
@@ -2,17 +2,19 @@ import axios from 'axios';
 import WebService from './WebService';
 
 const url = 'https://anapioficeandfire.com/api/characters';
+const urlFakeApi = 'https://reqres.in/api/';
 
 export default {
-	async postLogin({ character }) {
-		const user = await WebService.post(url + '583', {
-			character
+	async createFakeApi({ name, job }) {
+		return await WebService.post(urlFakeApi + 'users', {
+			name,
+			job
 		});
-		return {
-			user
-		};
 	},
 	async getCharacter({ character }) {
 		return await WebService.get(url + '/' + character);
+	},
+	async getListUsers() {
+		return await WebService.get(urlFakeApi + 'users?page=2');
 	}
 };
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import styles from './Home.module.scss';
 import WebServices from '../../WebServices/WebServices';
+import produce from 'immer/dist/immer';
+import { aliases } from './Helper';
 
 export default (class Home extends React.PureComponent {
-	state = {};
+	state = {
+		response: {}
+	};
 
 	componentDidMount() {
 		this.fetchData();
@@ -12,16 +16,66 @@ export default (class Home extends React.PureComponent {
 	fetchData = async () => {
 		try {
 			const character = '583';
+			const name = 'morpheus';
+			const job = 'leader';
 			const response = await WebServices.getCharacter({
 				character
 			});
-			console.log('TCL: Home -> fetchData -> response', response);
+
+			const response2 = await WebServices.getListUsers({});
+			console.log('TCL: Home -> fetchData -> response2', response2);
+
+			const response3 = await WebServices.createFakeApi({
+				name: 'walter',
+				job
+			});
+			console.log('TCL: Home -> fetchData -> response3', response3);
+			const nextState = produce(this.state, (draft) => {
+				draft.response = response;
+			});
+			this.setState(nextState);
+			aliases(response.aliases);
 		} catch (e) {
 			console.log('TCL: Home -> fetchData -> e', e);
 		}
 	};
 
 	render() {
-		return <div className={styles.main}>Home</div>;
+		const { response } = this.state;
+		console.log('TCL: Home -> render -> response', response);
+
+		return (
+			<div className={styles.main}>
+				<div>
+					<p>Nombre: {response && response.name}</p>
+				</div>
+
+				{response &&
+				response.playedBy &&
+				response.playedBy.length > 0 && (
+					<div>
+						<p>Actores: </p>
+						{response.playedBy.map((item) => {
+							return <p>{item}</p>;
+						})}
+					</div>
+				)}
+
+				{response &&
+				response.aliases &&
+				response.aliases.length > 0 && (
+					<div>
+						<p>Actores: </p>
+						{response.aliases.map((item) => {
+							return <p>{item}</p>;
+						})}
+					</div>
+				)}
+
+				<div>
+					<p>Apodos: {response && response.aliases}</p>
+				</div>
+			</div>
+		);
 	}
 });
