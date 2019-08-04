8eb7b7bdee32bbfcc9bd50e39a41af527d813568
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2019-08-04 09:27:59 -0500

    PDF button added

--- src/screens/Users/Users.js
+++ src/screens/Users/Users.js
@@ -21,7 +21,9 @@ export default (class Users extends React.PureComponent {
 			// 	draft.response = response;
 			// });
 			// this.setState(nextState);
-			this.setState({ response: response[0] });
+			this.setState({
+				response: response[0]
+			});
 
 			console.log('TCL: fetchData -> response', response);
 		} catch (e) {}
@@ -31,41 +33,52 @@ export default (class Users extends React.PureComponent {
 		console.log('TCL: showData -> country', country);
 		this.fetchData(country);
 	};
+
+	generatePDF = () => {
+		console.log('TCL: generatePDF');
+	};
 	render() {
 		const { response, countries } = this.state;
 		return (
 			<div className={styles.main}>
-				<ul>
-					{countries.map((country, i) => {
-						return (
-							<li key={i} className={styles.country} onClick={() => this.showData(country)}>
-								{country}
-							</li>
-						);
-					})}
-				</ul>
-				<ul>
-					<li> País: {response && response.name}</li>
-					<li> Capital: {response && response.capital}</li>
-					<li> Población: {response && response.population}</li>
-					<li>
-						Idioma:
-						{response &&
-							response.languages &&
-							response.languages.map((item, i) => {
-								return response.languages.length > 1 && i !== response.languages.length - 1 ? ' ' + item.nativeName + ', ' : ' ' + item.nativeName;
-							})}
-					</li>
-					<li> Región: {response && response.region}</li>
-					<li>
-						Monedas:
-						{response &&
-							response.currencies &&
-							response.currencies.map((item, i) => {
-								return response.currencies.length > 1 && i !== response.currencies.length - 1 ? ' ' + item.name + ', ' : ' ' + item.name;
-							})}
-					</li>
-				</ul>
+				<div className={styles.data}>
+					<ul>
+						{countries.map((country, i) => {
+							return (
+								<li key={i} className={styles.country} onClick={() => this.showData(country)}>
+									{country}
+								</li>
+							);
+						})}
+					</ul>
+					<ul>
+						<li> País: {response && response.name}</li>
+						<li> Capital: {response && response.capital}</li>
+						<li> Población: {response && response.population}</li>
+						<li>
+							Idioma:
+							{response &&
+								response.languages &&
+								response.languages.map((item, i) => {
+									return response.languages.length > 1 && i !== response.languages.length - 1 ? ' ' + item.nativeName + ', ' : ' ' + item.nativeName;
+								})}
+						</li>
+						<li> Región: {response && response.region}</li>
+						<li>
+							Monedas:
+							{response &&
+								response.currencies &&
+								response.currencies.map((item, i) => {
+									return response.currencies.length > 1 && i !== response.currencies.length - 1 ? ' ' + item.name + ', ' : ' ' + item.name;
+								})}
+						</li>
+					</ul>
+				</div>
+				<div className={styles.container_button}>
+					<button className={styles.button} onClick={() => this.generatePDF()}>
+						PDF
+					</button>
+				</div>
 			</div>
 		);
 	}
